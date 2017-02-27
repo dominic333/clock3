c2abfa06251e5354a3a87bdd18c28384498a1fb8
k3v1ndz <kevinmaulana1991@gmail.com>
2017-02-27 14:33:50 +0700

    Style Leave Calendar

--- application/modules/selfieattendance/views/monthview.php
+++ application/modules/selfieattendance/views/monthview.php
@@ -1,21 +1,22 @@
-    <div class="contentfirst">
+<div class="contentfirst">
 
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <h1>
-                My Attendance
-                <small>Calendar View</small>
-            </h1>
-            <ol class="breadcrumb">
-                <li><a href="<?php echo base_url();?>selfiemyaccount/account"><i class="fa fa-calendar"></i> Account</a></li>
-                <li><a href="<?php echo base_url();?>selfieattendance/attendance"> My Attendance</a></li>
-            </ol>
-        </section>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            My Attendance
+            <small>Calendar View</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="<?php echo base_url(); ?>selfiemyaccount/account"><i class="fa fa-calendar"></i> Account</a>
+            </li>
+            <li><a href="<?php echo base_url(); ?>selfieattendance/attendance"> My Attendance</a></li>
+        </ol>
+    </section>
 
-        <!-- Main content -->
-		<section class="content">
-			<div class="row">
-				 <?php /* $leaveManagement= $this->authentication->checkLeaveManagementAccess(); if($leaveManagement==1){ ?>
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <?php /* $leaveManagement= $this->authentication->checkLeaveManagementAccess(); if($leaveManagement==1){ ?>
              <div class="col-md-3">
                 <div class="box box-solid">
                     <div class="box-header with-border">
@@ -43,108 +44,162 @@
 				<?php } else { ?>
 				<div class="col-md-12">
 				<?php } ?>
-<?php */?>
+<?php */ ?>
 
-				<div id="exTab1" class="col-md-12">
-					<ul class="nav nav-pills">
-						<li class="active border-btn">
-							<a href="#1a" data-toggle="tab">My Attendance</a>
-						</li>
-						<li class="border-btn">
-							<a href="#2a" data-toggle="tab">Leave Calendar</a>
-						</li>
-					</ul>
-					<br>
+            <div id="exTab1" class="col-md-12">
+                <ul class="nav nav-pills">
+                    <li class="active border-btn">
+                        <a href="#1a" data-toggle="tab">My Attendance</a>
+                    </li>
+                    <li class="border-btn">
+                        <a href="#2a" data-toggle="tab">Leave Calendar</a>
+                    </li>
+                </ul>
+                <br>
 
-					<div class="tab-content clearfix">
-						<div class="tab-pane active" id="1a">
+                <div class="tab-content clearfix">
+                    <div class="tab-pane active" id="1a">
 
-							<div class="row">
-								<div class="col-md-12">
-										<div class="box box-primary">
-											<div class="box-body no-padding">
-												<!-- THE CALENDAR -->
-												<div id="calendar"></div>
-											</div>
-											<!-- /.box-body -->
-										</div>
-										<!-- /. box -->
-								</div>
-								<!-- /.col-md-12 -->
-							</div>
-							<!-- /.row -->
-
-
-						</div>
-						<div class="tab-pane" id="2a">
-							<div class="row">
-								<div class="col-md-12">
-									<div id="toggle" class="article">
-										<h3><span>Leave Calendar</span></h3>
-										<h4>You can toggle date and get those dates as array.</h4>
-										<div class="toggle-calendar"></div>
-										<div class="box"></div>
-
-									</div>
-
-								</div>
-								<!-- /.col-md-12 -->
-							</div>
-
-							<div class="row">
-								<div class="col-md-12">
-									<h4>Step 2: Add Note & Select Leave Type</h4>
-									<div class="box box-default">
-										<div class="box-header with-border">
-											<h3 class="box-title">Fill Form</h3>
-										</div>
-										<form id="formLeaveRequest2" name="formLeaveRequest2" method="POST" class="form-horizontal">
-											<div class="box-body">
-												<div class="form-group">
-													<label for="inputEmail3" class="col-sm-2 control-label">Add Note</label>
-
-													<div class="col-sm-10">
-														<textarea class="form-control" rows="3" name="leaveNote" id="leaveNote" ></textarea>
-													</div>
-												</div>
-												<div class="form-group">
-													<label class="col-sm-2 control-label">Leave Type</label>
-													<div class="col-sm-10">
-														<select id="leavetype" name="leavetype" class="form-control">
-															<option value="">Select a Leave Form</option>
-															<option value="medical">Medical Leave</option>
-															<option value="casual">Casual Leave</option>
-															<option value="annual">Annual Leave</option>
-														</select>
-													</div>
-												</div>
-											</div>
-											<div class="box-footer">
-												<button type="submit" class="btn btn-success pull-right">Submit</button>
-											</div>
-										</form>
-									</div>
-								</div>
-
-							</div>
-							<!-- /.row -->
-
-						</div>
-					</div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="box box-primary">
+                                    <div class="box-body no-padding">
+                                        <!-- THE CALENDAR -->
+                                        <div id="calendar"></div>
+                                    </div>
+                                    <!-- /.box-body -->
+                                </div>
+                                <!-- /. box -->
+                            </div>
+                            <!-- /.col-md-12 -->
+                        </div>
+                        <!-- /.row -->
 
 
+                    </div>
+                    <div class="tab-pane" id="2a">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div id="toggle" class="article">
+                                    <div class="box box-danger">
+                                        <div class="box-header with-border">
+                                            <h3 class="box-title">Leave Calendar</h3>
+                                            <h5>You can toggle date and get those dates as array.</h5>
+
+                                            <div class="box-tools pull-right">
+                                                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
+                                                        class="fa fa-minus"></i></button>
+                                            </div>
+                                        </div>
+
+                                        <div class="box-body">
+                                            <div class="toggle-calendar"></div>
+                                            <div></div>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="box box-danger">
+                                    <div class="box-header with-border">
+                                        <h3 class="box-title">Step 2: Add Note & Select Leave Type</h3>
+                                        <h5>Fill Form</h5>
+                                        <div class="box-tools pull-right">
+                                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
+                                                    class="fa fa-minus"></i></button>
+                                        </div>
+                                    </div>
+                                    <div class="box-body">
+                                        <form id="formLeaveRequest2" name="formLeaveRequest2" method="POST"
+                                              class="form-horizontal">
+                                            <div class="box-body">
+                                                <div class="form-group">
+                                                    <label for="inputEmail3" class="col-sm-2 control-label">Add
+                                                        Note</label>
+
+                                                    <div class="col-sm-10">
+                                                    <textarea class="form-control" rows="3" name="leaveNote"
+                                                              id="leaveNote"></textarea>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="col-sm-2 control-label">Leave Type</label>
+                                                    <div class="col-sm-10">
+                                                        <select id="leavetype" name="leavetype" class="form-control">
+                                                            <option value="">Select a Leave Form</option>
+                                                            <option value="medical">Medical Leave</option>
+                                                            <option value="casual">Casual Leave</option>
+                                                            <option value="annual">Annual Leave</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="box-footer">
+                                                <button type="submit" class="btn btn-success pull-right">Submit</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="row">
+                            <!--                            <div class="col-md-12">-->
+                            <!--                                <h4>Step 2: Add Note & Select Leave Type</h4>-->
+                            <!--                                <div class="box box-default">-->
+                            <!--                                    <div class="box-header with-border">-->
+                            <!--                                        <h3 class="box-title">Fill Form</h3>-->
+                            <!--                                    </div>-->
+                            <!--                                    <form id="formLeaveRequest2" name="formLeaveRequest2" method="POST"-->
+                            <!--                                          class="form-horizontal">-->
+                            <!--                                        <div class="box-body">-->
+                            <!--                                            <div class="form-group">-->
+                            <!--                                                <label for="inputEmail3" class="col-sm-2 control-label">Add Note</label>-->
+                            <!---->
+                            <!--                                                <div class="col-sm-10">-->
+                            <!--                                                    <textarea class="form-control" rows="3" name="leaveNote"-->
+                            <!--                                                              id="leaveNote"></textarea>-->
+                            <!--                                                </div>-->
+                            <!--                                            </div>-->
+                            <!--                                            <div class="form-group">-->
+                            <!--                                                <label class="col-sm-2 control-label">Leave Type</label>-->
+                            <!--                                                <div class="col-sm-10">-->
+                            <!--                                                    <select id="leavetype" name="leavetype" class="form-control">-->
+                            <!--                                                        <option value="">Select a Leave Form</option>-->
+                            <!--                                                        <option value="medical">Medical Leave</option>-->
+                            <!--                                                        <option value="casual">Casual Leave</option>-->
+                            <!--                                                        <option value="annual">Annual Leave</option>-->
+                            <!--                                                    </select>-->
+                            <!--                                                </div>-->
+                            <!--                                            </div>-->
+                            <!--                                        </div>-->
+                            <!--                                        <div class="box-footer">-->
+                            <!--                                            <button type="submit" class="btn btn-success pull-right">Submit</button>-->
+                            <!--                                        </div>-->
+                            <!--                                    </form>-->
+                            <!--                                </div>-->
+                            <!--                            </div>-->
+
+                        </div>
+                        <!-- /.row -->
+
+                    </div>
+                </div>
 
 
+            </div>
 
-				</div>
-
-				<!-- /.col -->
-			</div>
-			<!-- /.row -->
-		</section>
-        <!-- /.content -->
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
 
 
-        </section>
+    </section>
 
-    </div>
+</div>
--- assets/common/pignose_settings.js
+++ assets/common/pignose_settings.js
@@ -3,49 +3,49 @@
  *Modified by gayatri - disable past days  and non-working days
  */
 
-$(function() {
+$(function () {
 
-    var dateArray =[];
+    var dateArray = [];
     $('#wrapper .version strong').text('v' + pignoseCalendar.VERSION);
     // Toggle type Calendar
     /*
-	 var post_url = base_url+"selfieattendance/attendance/getNonWorkingDays"; 
-	 var offDays = [] ;
-    $.ajax({
-    	
-  			url:post_url,
-    		data:
-                    {
-                         csrf_test_name : csrf_token
-                    },
-         type: "get",
-         dataType: 'json' ,
-       
-    		success: function (result) {
-         
-			$.each(result.days, function (i, d) {
-					var date = new Date(d).toISOString().slice(0, 10);
-					alert(date);
-					offDays.push(date);
-			}); 
-			    			//a[0]= result.sunday
-    			
-    			//days.push(result);
-    			//console.log(result);
-    		}
-    });
-    
-   */
+     var post_url = base_url+"selfieattendance/attendance/getNonWorkingDays";
+     var offDays = [] ;
+     $.ajax({
 
-    
+     url:post_url,
+     data:
+     {
+     csrf_test_name : csrf_token
+     },
+     type: "get",
+     dataType: 'json' ,
+
+     success: function (result) {
+
+     $.each(result.days, function (i, d) {
+     var date = new Date(d).toISOString().slice(0, 10);
+     alert(date);
+     offDays.push(date);
+     });
+     //a[0]= result.sunday
+
+     //days.push(result);
+     //console.log(result);
+     }
+     });
+
+     */
+
+
     $('.toggle-calendar').pignoseCalendar({
         toggle: true,
-        minDate:moment(),
-        disabledWeekdays :[],
-        select: function(date, obj) {
-            var selDate= date[0].format('YYYY-MM-DD');
-            if(jQuery.inArray(selDate, dateArray) != -1) {
-                dateArray = jQuery.grep(dateArray, function(value) {
+        minDate: moment(),
+        disabledWeekdays: [],
+        select: function (date, obj) {
+            var selDate = date[0].format('YYYY-MM-DD');
+            if (jQuery.inArray(selDate, dateArray) != -1) {
+                dateArray = jQuery.grep(dateArray, function (value) {
                     return value != selDate;
                 });
                 //console.log(dateArray);
@@ -53,19 +53,19 @@ $(function() {
                 dateArray.push(selDate);
                 //console.log(dateArray);
             }
-            var $target = obj.calendar.parent().next().show().html('You selected ' +
-                (date[0] === null? 'null':date[0].format('YYYY-MM-DD')) +
+            var $target = obj.calendar.parent().next().show().html('<center><br>You selected<br><h3> ' +
+                (date[0] === null ? 'null' : date[0].format('YYYY-MM-DD</h3></center>')) +
                 '.' +
                 '<br /><br />' +
-                '<strong>Active dates</strong><br /><br />' +
+                'Active dates<br /><br />' +
                 '<div class="active-dates"></div>');
 
-            for(var idx in obj.storage.activeDates) {
+            for (var idx in obj.storage.activeDates) {
 
                 var date = obj.storage.activeDates[idx];
                 //console.log(date);
                 //dateArray.push(date);
-                if(typeof date !== 'string') {
+                if (typeof date !== 'string') {
                     continue;
                 }
                 $target.find('.active-dates').append('<span class="ui label default">' + date + '</span>');
@@ -80,28 +80,24 @@ $(function() {
     //Dominic, Feb 21,2017
     $('#formLeaveRequest2').validate(
         {
-            rules:
-            {
-                leaveNote:
-                {
+            rules: {
+                leaveNote: {
                     required: true
                 },
-                leavetype:
-                {
+                leavetype: {
                     required: true
                 }
             },
-            highlight: function(element) {
+            highlight: function (element) {
                 $(element).closest('.control-group').removeClass('success').addClass('error');
             },
-            success: function(element) {
+            success: function (element) {
                 element
                     .text('').addClass('valid')
                     .closest('.control-group').removeClass('error').addClass('success');
             },
-            submitHandler: function(form)
-            {
-                var post_url = base_url+"selfieattendance/attendance/applyForLeave";
+            submitHandler: function (form) {
+                var post_url = base_url + "selfieattendance/attendance/applyForLeave";
                 var leaveType = $.trim($("#formLeaveRequest2 #leavetype").val());
                 var leaveNote = $.trim($("#formLeaveRequest2 #leaveNote").val());
                 var leaveDates = dateArray;
@@ -110,42 +106,38 @@ $(function() {
 
                 $.ajax({
                     url: post_url,
-                    data:
-                    {
-                        leaveType : leaveType,
-                        leaveNote : leaveNote,
-                        leaveDates : leaveDates,
-                        csrf_test_name : csrf_token
+                    data: {
+                        leaveType: leaveType,
+                        leaveNote: leaveNote,
+                        leaveDates: leaveDates,
+                        csrf_test_name: csrf_token
                     },
                     type: "POST",
                     dataType: 'HTML',
-                    beforeSend: function ( xhr ) {
+                    beforeSend: function (xhr) {
                         //Add your image loader here
                         showLoader();
                     },
-                    success: function(result)
-                    {
+                    success: function (result) {
                         hideLoader();
-                        var result= result.trim();
+                        var result = result.trim();
 
-                        if(result=="success")
-                        {
+                        if (result == "success") {
                             $.alert({
                                 title: 'Leaves Applied!',
                                 content: 'Your leave request has been placed.',
-                                confirm: function(){
+                                confirm: function () {
 
                                 }
                             });
 //                            alert("Leave Applied");
 
                         }
-                        else if(result=="failed")
-                        {
+                        else if (result == "failed") {
                             $.alert({
                                 title: 'Sorry!',
                                 content: 'Unable to process your leave request. Please try again',
-                                confirm: function(){
+                                confirm: function () {
 
                                 }
                             });
