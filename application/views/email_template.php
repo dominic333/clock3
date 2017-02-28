55a70fc3c5c54d0090fa2b25fb1950680ffe398e
k3v1ndz <kevinmaulana1991@gmail.com>
2017-02-28 16:48:41 +0700

    design email template

--- application/views/email_template.php
+++ application/views/email_template.php
@@ -1,248 +1,192 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <title>Email Template</title>
-      <style type="text/css">
-      body {
-       padding-top: 0 !important;
-       padding-bottom: 0 !important;
-       padding-top: 0 !important;
-       padding-bottom: 0 !important;
-       margin:0 !important;
-       width: 100% !important;
-       -webkit-text-size-adjust: 100% !important;
-       -ms-text-size-adjust: 100% !important;
-       -webkit-font-smoothing: antialiased !important;
-     }
-     .tableContent img {
-       border: 0 !important;
-       display: block !important;
-       outline: none !important;
-     }
-     a{
-      color:#382F2E;
-    }
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;">
+    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE"/>
+    <meta name="format-detection" content="telephone=no">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="format-detection" content="date=no">
+    <meta name="format-detection" content="address=no">
+    <meta name="format-detection" content="email=no">
+    <title>Email Template</title>
 
-    p, h1{
-      color:#382F2E;
-      margin:0;
-    }
- p{
-      text-align:left;
-      color:#999999;
-      font-size:14px;
-      font-weight:normal;
-      line-height:19px;
-    }
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            min-width: 100%;
+            width: 100% !important;
+            height: 100% !important;
+            background-color: #EEEEEE;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
 
-    a.link1{
-      color:#382F2E;
-    }
-    a.link2{
-      font-size:16px;
-      text-decoration:none;
-      color:#ffffff;
-    }
+        body, table, td, div, p, a {
+            -webkit-font-smoothing: antialiased;
+            text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+            line-height: 100%;
+        }
 
-    h2{
-      text-align:left;
-       color:#222222; 
-       font-size:17px;
-      font-weight:normal;
-    }
-    div,p,ul,h1{
-      margin:0;
-    }
+        table, td {
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+            border-collapse: collapse !important;
+            border-spacing: 0;
+        }
 
-    .bgBody{
-      background: #ffffff;
-    }
-    .bgItem{
-      background: #ffffff;
-    }
+        img {
+            max-width: 100%;
+            height: auto;
+        }
 
+        #outlook a {
+            padding: 0;
+        }
+
+        @media all and (min-width: 560px) {
+            .container {
+                border-radius: 8px;
+                -webkit-border-radius: 8px;
+                -moz-border-radius: 8px;
+                -khtml-border-radius: 8px;
+            }
+        }
+
+        a, a:hover {
+            color: #127DB3;
+        }
+
+        .footer a, .footer a:hover {
+            color: #999999;
+        }
+
+        .table-main {
+            border-collapse: collapse;
+            border-spacing: 0;
+            padding: 0;
+            width: inherit;
+            max-width: 660px;
+        }
+
+        .label {
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        }
+
+        .label-default {
+            background-color: #777;
+        }
+
+        .label-warning {
+            background-color: #f0ad4e;
+        }
+
+        .label-success {
+            background-color: #5cb85c;
+        }
+
+        .label-primary {
+            background-color: #337ab7;
+        }
+
+        .label-danger {
+            background-color: #d9534f;
+        }
+
+        /*.label {*/
+        /*display: inline;*/
+        /*padding: .2em .6em .3em;*/
+        /*font-size: 95%;*/
+        /*font-weight: 700;*/
+        /*line-height: 1;*/
+        /*color: #fff;*/
+        /*text-align: center;*/
+        /*white-space: nowrap;*/
+        /*vertical-align: baseline;*/
+        /*border-radius: .25em;*/
+        /*}*/
     </style>
-<script type="colorScheme" class="swatch active">
-{
-    "name":"Default",
-    "bgBody":"ffffff",
-    "link":"382F2E",
-    "color":"999999",
-    "bgItem":"ffffff",
-    "title":"222222"
-}
-</script>
-  </head>
-	<body paddingwidth="0" paddingheight="0"   style="padding-top: 0; padding-bottom: 0; padding-top: 0; padding-bottom: 0; background-repeat: repeat; width: 100% !important; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased;" offset="0" toppadding="0" leftpadding="0">
-		<table width="100%" border="0" cellspacing="0" cellpadding="0" class="tableContent bgBody" align="center"  style='font-family:Helvetica, Arial,serif;'>
-			<tr><td height='35'></td></tr>
 
-      	<tr>
-        		<td>
-          	<table width="500" border="0" cellspacing="0" cellpadding="0" align="center" class='bgItem'>
-            	<tr>
-              		<td width='40'></td>
-              		<td width='450'>
-                	<table width="450" border="0" cellspacing="0" cellpadding="0" align="center">
+</head>
 
-<!-- =============================== Header ====================================== -->           
-                  
 
-                  <tr><td height='75'></td></tr>
-<!-- =============================== Body ====================================== -->
+<body>
 
-                  <tr>
-                    <td class='movableContentContainer' valign='top' style="background-color: #F5F5F5;padding: 30px;">
-                        
-                      <div class='movableContent'>
-                        <table width="450" border="0" cellspacing="0" cellpadding="0" align="center">
-                          <tr>
-                            <td valign='top' align='center'>
-                              <div class="contentEditableContainer contentImageEditable">
-                                <div class="contentEditable" style="margin-bottom: 20px;">
-                                  <img src="<?php echo base_url(); ?>images/site/main_logo.jpg" width='200' height='200' alt='' data-default="placeholder" data-max-width="560">
-                                </div>
-                              </div>
-                            </td>
-                          </tr>
-                        </table>
-                      </div>
-                        
-                      <div class='movableContent'>
-                        <table width="450" border="0" cellspacing="0" cellpadding="0" align="center">
-                          <tr>
-                            <td valign='top' align='center'>
-                              <div class="contentEditableContainer contentTextEditable">
-                                <div class="contentEditable">
-                                  <p style='text-align:center;margin:0;font-family:Georgia,Time,sans-serif;font-size: 23px;color: #222222;white-space: pre;'>Welcome to <span style='color:#DC2828;'>Brunei Inspection Portal Service</span></p>
-                                </div>
-                              </div>
-                            </td>
-                          </tr>
-                        </table>
-                      </div>
+<table border="0" cellpadding="0" cellspacing="0" align="center"
+       bgcolor="#FFFFFF"
+       width="660" class="container table-main">
 
-                      <div class='movableContent'>
-                        <table width="450" border="0" cellspacing="0" cellpadding="0" align="center">
-                          <tr><td height='25'style="border-bottom:1px solid #DDDDDD;"></td></tr>
-                          <tr>
-                            <td align='left'>
-                              <div class="contentEditableContainer contentTextEditable">
-                                <div class="contentEditable" align='center'>
-                                  <h2 >Hi <?php echo (isset($msg_header) ? $msg_header :''); ?>,</h2>
-                                </div>
-                              </div>
-                            </td>
-                          </tr>
-
-                          <tr><td height='15'> </td></tr>
-
-                          <tr>
-                            <td align='left'>
-                              <div class="contentEditableContainer contentTextEditable">
-                                <div class="contentEditable" align='center'>
-                                  <p  style='text-align:left;color:#999999;font-size:14px;font-weight:normal;line-height:19px;'>
-                                    <?php echo (isset($msg_body) ? $msg_body :''); ?>
-                                  </p>
-                                </div>
-                              </div>
-                            </td>
-                          </tr>
-
-                        <tr><td height='20'></td></tr>
-
-                            <!--<tr>
-                            <td align='center'>
-                              <table>
-                                <tr>
-                                  <td align='center' bgcolor='#DC2828' style='background:#B91D35; padding:15px 18px;-webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;'>
-                                    <div class="contentEditableContainer contentTextEditable">
-                                      <div class="contentEditable" align='center'>
-                                        <a target='_blank' href='#' class='link2' style='color:#ffffff;font-weight: bold;'>Reset your Password</a>
-                                      </div>
-                                    </div>
-                                  </td>
-                                </tr>
-                              </table>
-                            </td>
-                          </tr>-->
-                        </table>
-                      </div>
-
-                      <div lass='movableContent'>
-                        <table width="450" border="0" cellspacing="0" cellpadding="0" align="center">
-                          <tr><td height='35'></td></tr>
-                          <tr><td  style='border-bottom:1px solid #DDDDDD;'></td></tr>
-
-                          <tr><td height='25'></td></tr>
-
-                          <tr>
-                            <td>
-                              <table width="450" border="0" cellspacing="0" cellpadding="0" align="center">
-                                <tr>
-                                  <td valign='top' align='left' width='370'>
-                                    <div class="contentEditableContainer contentTextEditable">
-                                      <div class="contentEditable" align='center'>
-                                        <p  style='text-align:left;color:#CCCCCC;font-size:12px;font-weight:normal;line-height:20px;'>
-                                          <span style='font-weight:bold;'>BIPS</span>
-                                          <br>
-                                          Brunei City<br>
-                                          
-                                          <br>
-                                          <!--<a target='_blank' href="[UNSUBSCRIBE]" class='link1' class='color:#382F2E;'>Unsubscribe</a>-->
-                                        </p>
-                                      </div>
-                                    </div>
-                                  </td>
-
-                                  <td width='30'></td>
-
-                                  <td valign='top' width='52'>
-                                    <div class="contentEditableContainer contentFacebookEditable">
-                                      <div class="contentEditable">
-                                        <a target='_blank' href="#"><img src="<?php echo base_url(); ?>images/site/facebook.png" width='52' height='53' alt='facebook icon' data-default="placeholder" data-max-width="52" data-customIcon="true"></a>
-                                      </div>
-                                    </div>
-                                  </td>
-
-                                  <td width='16'></td>
-
-                                  <td valign='top' width='52'>
-                                    <div class="contentEditableContainer contentTwitterEditable">
-                                      <div class="contentEditable">
-                                        <a target='_blank' href="#"><img src="<?php echo base_url(); ?>images/site/twitter.png" width='52' height='53' alt='twitter icon' data-default="placeholder" data-max-width="52" data-customIcon="true"></a>
-                                      </div>
-                                    </div>
-                                  </td>
-                                </tr>
-                              </table>
-                            </td>
-                          </tr>
-                        </table>
-                      </div>
-
-                    </td>
-                  </tr>
-
-                  
-
-<!-- =============================== footer ====================================== -->
-                  
-               </table>
-              </td>
-              <td width='40'></td>
-            </tr>
-          </table>
+    <tr>
+        <td align="left" style="width: 30%; padding-left: 10px;  padding-top: 10px; padding-bottom: 10px;">
+            <img src="<?php echo base_url(); ?>images/site/logo_login2.png" alt="clock-in.me"/>
         </td>
-      </tr>
+        <td align="right"
+            style="width: 100%; padding-left: 10px;  padding-right: 10px; padding-top: 10px; padding-bottom: 10px;">
+            <span>www.clock-in.me</span> &nbsp; &nbsp; &nbsp; &nbsp; <span class="label"
+                                                                           style="font-size: 12px;">(US) +1 (206) 2587269</span>&nbsp;
+            <span class="label" style="font-size: 12px;">(AUS) +61 (39) 0216940</span>
+        </td>
+    </tr>
 
-      <tr><td height='88'></td></tr>
+    <tr>
+        <td colspan="3" align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+			padding-top: 0px;" class="line">
+            <hr
+                color="#E0E0E0" align="center" width="100%" size="1" noshade
+                style="margin: 0; padding: 0;"/>
+        </td>
+    </tr>
+
+    <tr>
+        <td colspan="3" align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;
+			padding-top: 25px;
+			color: #000000;
+			font-family: sans-serif;" class="paragraph">
+
+            <h1 style="font-size: 23px;"><span style="color: #f61d3a;"><strong><i>Congratulations !</i></strong></span>
+            </h1>
+            <p style="font-size: 27px">Everybody is on time</p>
+            <img src="<?php echo base_url(); ?>images/email-1.png" width="70px">
+            <img src="<?php echo base_url(); ?>images/email-2.png" width="70px">
+            <img src="<?php echo base_url(); ?>images/email-3.png" width="70px">
+            <img src="<?php echo base_url(); ?>images/email-4.png" width="70px">
+            <img src="<?php echo base_url(); ?>images/email-5.png" width="70px">
+        </td>
+    </tr>
 
 
-    </table>
+    <tr>
+        <td colspan="3" align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+			padding-top: 25px;" class="line">
+            <hr
+                color="#E0E0E0" align="center" width="100%" size="1" noshade
+                style="margin: 0; padding: 0;"/>
+        </td>
+    </tr>
 
-      </body>
-      </html>
+    <tr>
+        <td colspan="3" align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;
+			padding-top: 20px;
+			padding-bottom: 25px;
+			color: #000000;
+			font-family: sans-serif;" class="paragraph">
+            <p>Clock-in.me Customer Care Center</p>
+            <p style="font-size: 13px;">
+                Do contact us at <a href="mailto:cs@entreprenity.co" target="_blank"
+                                    style="color: #127DB3; font-family: sans-serif; font-weight: 400; line-height: 160%;">ask@clock-in.me</a>
+                if you require any assistance.
+            </p>
+            <p style="font-size: 13px;">
+                Clock-in.me Support Team :)
+            </p>
+        </td>
+    </tr>
+</table>
 
-
+</body>
+</html>
