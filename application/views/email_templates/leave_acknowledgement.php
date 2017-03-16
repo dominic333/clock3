40f803d8917e32522bb8c69bb4b383bad9fee6bb
k3v1ndz <kevinmaulana1991@gmail.com>
2017-03-16 16:22:37 +0700

    logo email

--- application/views/email_templates/leave_acknowledgement.php
+++ application/views/email_templates/leave_acknowledgement.php
@@ -123,7 +123,8 @@
 
     <tr>
         <td align="left" style="width: 30%; padding-left: 10px;  padding-top: 10px; padding-bottom: 10px;">
-            <img src="https://clock-in.me/webapp/images/logo_login2.png" alt="clock-in.me" width="100px"/>
+<!--            <img src="https://clock-in.me/webapp/images/logo_login2.png" alt="clock-in.me"/>-->
+            <img src="<?php echo $baseurl;?>assets/cc/images/logo_login3.png" alt="clock-in.me"/>
         </td>
         <td align="right"
             style="width: 100%; padding-left: 10px;  padding-right: 10px; padding-top: 10px; padding-bottom: 10px;">
